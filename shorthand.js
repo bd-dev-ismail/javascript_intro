c6c732a7fb6971e4976f45858023b51ae5e3775e
bd-dev-ismail <ismailratul7@gmail.com>
2022-07-31 05:25:54 +0600

    changing variable

---
+++ shorthand.js
@@ -0,0 +1,26 @@
+//variable diclartion 
+var mangoPrice = 55;
+//variable value update
+mangoPrice = 110;
+//add something with perivious value
+var price1 = 65;
+var price2 = 48;
+// price1 = price1 + 2;
+// console.log(price1);
+price1 += 10;
+// console.log(price1);
+// price2 = price2 - 10;
+// price2 -= 10;
+// console.log(price2);
+// price2 *= 2;
+// console.log(price2);
+// price2 /= 2;
+// console.log(price2);
+var age = 19;
+// ++ means add 1 with the existing value
+age++;
+console.log(age);
+var money = 200;
+//deducating 1 from the existing value
+money--;
+console.log(money);
