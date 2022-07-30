5f27b74ec86e6c0786666fc8281e8967a75fd415
bd-dev-ismail <ismailratul7@gmail.com>
2022-07-30 22:58:18 +0600

    commit

--- variable.js
+++ variable.js
@@ -6,14 +6,17 @@ var temperture = 38;
 var weight = 50;
 var money = 70;
 var children = 2;
+var salary = 9000;
 // String type 
 var person = "Md Ismail Hossen" ;
 var location = "Kumira, Chittagong" ;
 var special = "Nowrin" ;
 var location = "Bashbaria" ;
+var dream = "Wev Developer" ;
 
 // Boolean 
 var serious = true;
 var isFullMarks = true;
 var isDiamondsFull = false;
 var isMarride = false;
+var isRomance = true;
--- variablename.js
+++ variablename.js
@@ -1,5 +1,6 @@
-var price = 45;
+var iprce = 45;
 var batch2018 = 34;
 var userName = "Md Ismail Hossen"; //camel case 
 var user_email_is = "@gmail.com" ; //snake case
 var UserEmailIs = "gs@info.bd" ; //pascal case
+var userInput = "Hello World" ;
