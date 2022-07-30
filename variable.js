cc9f2d19e3cac6a90cd16080964fd9bbe4e4c590
bd-dev-ismail <ismailratul7@gmail.com>
2022-07-30 22:32:16 +0600

    teaching variable naming

--- variable.js
+++ variable.js
@@ -1,7 +1,19 @@
 // vary + able = variable 
+// Neumric 
 var price = 12;
 var age = 20;
 var temperture = 38;
 var weight = 50;
 var money = 70;
 var children = 2;
+// String type 
+var person = "Md Ismail Hossen" ;
+var location = "Kumira, Chittagong" ;
+var special = "Nowrin" ;
+var location = "Bashbaria" ;
+
+// Boolean 
+var serious = true;
+var isFullMarks = true;
+var isDiamondsFull = false;
+var isMarride = false;
---
+++ variablename.js
@@ -0,0 +1,5 @@
+var price = 45;
+var batch2018 = 34;
+var userName = "Md Ismail Hossen"; //camel case 
+var user_email_is = "@gmail.com" ; //snake case
+var UserEmailIs = "gs@info.bd" ; //pascal case
