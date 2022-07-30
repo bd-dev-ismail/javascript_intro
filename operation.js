6dace7079d11ba2fc9ba45aebc5b1688cb5be99c
bd-dev-ismail <ismailratul7@gmail.com>
2022-07-30 22:55:34 +0600

    applying operation

---
+++ operation.js
@@ -0,0 +1,23 @@
+var secloPrice = 50;
+var sergelPrice = 40;
+//Addition & Subtraciton 
+var totalPrice = secloPrice + sergelPrice;
+var priceDifference = secloPrice - sergelPrice;
+// var priceMultiple = secloPrice * sergelPrice;
+// console.log(priceMultiple);
+// console.log(priceDifference);
+// console.log(totalPrice);
+// console.log(secloPrice);
+// console.log(sergelPrice);
+// console.log(secloPrice + sergelPrice);
+// console.log(secloPrice * sergelPrice);
+//Multiplication 
+var priceOrange = 20;
+var quantity = 7;
+var totalCost = priceOrange * quantity;
+console.log(totalCost);
+//Division 
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
