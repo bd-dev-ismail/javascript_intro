29d55b02986111477ac93b77b5b9a1c1ab3163b8
bd-dev-ismail <ismailratul7@gmail.com>
2022-07-31 05:52:55 +0600

    conversion

---
+++ convertion.js
@@ -0,0 +1,15 @@
+var price1 = '34';
+var price2 =  '43';
+var totalPrice = price1 + price2;
+// console.log(totalPrice);
+var price1 = 34;
+var price2 =  '43';
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+//convert string to number
+console.log(price1);
+var price1Number = parseInt(price2);
+console.log(price1Number);
+var nowrinGpa = '4.78';
+var gpa = parseFloat(nowrinGpa);
+console.log(gpa);
