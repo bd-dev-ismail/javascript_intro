f5a1bd52a831da7679455b1b9c209fc1a86b5785
bd-dev-ismail <ismailratul7@gmail.com>
2022-07-31 06:17:26 +0600

    tutorial 1

---
+++ reminder.js
@@ -0,0 +1,3 @@
+var mango = 13;
+var person = 2;
+console.log(mango/person);
---
+++ types.js
@@ -0,0 +1,12 @@
+var price = 399;
+// console.log(typeof price);
+var isHappy = true;
+// console.log(isHappy);
+var romantic;
+// console.log(typeof romantic);
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(3);
+sum = parseFloat(sum);
+console.log(sum);
